eadc4a09810aaaf3629f4742cd425442bc39260d
Armando Herrera <anhydrous99@gmail.com>
2019-06-19 14:25:09 -0500

    Some changes

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python train.py --gpu_list 0,1 --input_size=512 --batch_size=26 --nb_workers=12 --training_data_path=data/train_data/ --validation_data_path=data/validation_data/ --checkpoint_path=tmp/cocotext_icdar2013_15_east_mobilenet_v2_11_06_2019/
+python train.py --gpu_list 0,1 --input_size=512 --batch_size=24 --nb_workers=10 --training_data_path=data/train_data/ --validation_data_path=data/validation_data/ --checkpoint_path=tmp/cocotext_icdar2013_15_east_mobilenet_v2_11_06_2019/
